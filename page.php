b74eb7507fbfd73153b356e0c3420d7c6ea4a27d
Shish <webmaster@shishnet.org>
2006-06-16 20:41:30 +0000

    pages use box.foot

--- page.php
+++ page.php
@@ -3,19 +3,18 @@
 <?php get_sidebar(); ?>
 
 	<div id="content" class="narrowcolumn">
-
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 		<div class="box" id="post-<?php the_ID(); ?>">
-		<h3><?php the_title(); ?></h3>
+			<h3><?php the_title(); ?></h3>
 			<div class="boxbody">
 				<?php the_content('<p class="serif">Read the rest of this page &raquo;</p>'); ?>
-	
 				<?php link_pages('<p><strong>Pages:</strong> ', '</p>', 'number'); ?>
-	
 			</div>
+	  	<div class="boxfoot">
+				<?php edit_post_link('Edit this entry.', '', ''); ?>
+		  </div>
 		</div>
 	  <?php endwhile; endif; ?>
-	<?php edit_post_link('Edit this entry.', '<p>', '</p>'); ?>
 	</div>
 
 <?php get_footer(); ?>
